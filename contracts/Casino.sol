0afefb4e825b71b469a8d5abd4f98d8a8de197c2
christophermayfield <christopher.j.mayfield@gmail.com>
2018-02-08 22:17:37 -0800

    initial commit

---
+++ contracts/Casino.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.11;
+
+contract Casino {
+   address owner;
+   function Casino(){
+      owner = msg.sender;
+   }
+   function kill(){
+      if(msg.sender == owner)
+         selfdestruct(owner);
+   }
+}
