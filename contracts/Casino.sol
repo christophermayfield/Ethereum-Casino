bf24aaf8faad6d8ec2c7c61ac9fc8ea391f6b5be
christophermayfield <christopher.j.mayfield@gmail.com>
2018-02-08 23:33:14 -0800

    add payable function

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -26,3 +26,15 @@ function Casino(uint _minimumBet){
    owner = msg.sender;
    if(_minimumBet != 0 ) minimumBet = _minimumBet;
 }
+
+// To bet for a number between 1 and 10 both inclusive
+function bet(uint number) payable{
+   assert(checkPlayerExists(msg.sender) == false);
+   assert(number >= 1 && number <= 10);
+   assert(msg.value >= minimumBet);
+   playerInfo[msg.sender].amountBet = msg.value;
+   playerInfo[msg.sender].numberSelected = number;
+   numberOfBets += 1;
+   players.push(msg.sender);
+   totalBet += msg.value;
+}
