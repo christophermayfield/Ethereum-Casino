77de080c4c35858c1b1de71630da5e4d9d8b55ce
christophermayfield <christopher.j.mayfield@gmail.com>
2018-02-08 23:53:15 -0800

    add winning ether function

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -54,3 +54,23 @@ function generateNumberWinner(){
    uint numberGenerated = block.number % 10 + 1; // This isn't secure
    distributePrizes(numberGenerated);
 }
+
+
+// Sends the corresponding ether to each winner depending on the total bets
+function distributePrizes(uint numberWinner){
+   address[100] memory winners; // We have to create a temporary in memory array with fixed size
+   uint count = 0; // This is the count for the array of winners
+   for(uint i = 0; i < players.length; i++){
+      address playerAddress = players[i];
+      if(playerInfo[playerAddress].numberSelected == numberWinner){
+         winners[count] = playerAddress;
+         count++;
+      }
+      delete playerInfo[playerAddress]; // Delete all the players
+   }
+   players.length = 0; // Delete all the players array
+   uint winnerEtherAmount = totalBet / winners.length; // How much each winner gets
+   for(uint j = 0; j < count; j++){
+      if(winners[j] != address(0)) // Check that the address in this fixed array is not empty
+         winners[j].transfer(winnerEtherAmount);
+   }
